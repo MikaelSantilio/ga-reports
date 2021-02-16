979e8921500efa65da3bd890abdb008a28fa5f8e
MikaelSantilio <mikael.santilio@gmail.com>
2021-02-16 07:48:04 -0300

    tested google api flow

--- ga_reports/core/urls.py
+++ ga_reports/core/urls.py
@@ -5,4 +5,6 @@ app_name = "core"
 
 urlpatterns = [
     path('oauth2callback', views.get_google_token, name='get_google_token'),
+    path('slide', views.test_google_api, name='test_google_api'),
+    path('auth', views.google_auth, name='google_auth'),
 ]
--- ga_reports/core/views.py
+++ ga_reports/core/views.py
@@ -18,6 +18,9 @@ from django.views.generic import (
 )
 from django.views.generic.base import ContextMixin, TemplateResponseMixin
 from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+import pdb
 
 
 class GoogleAuthorizationView(TemplateResponseMixin, ContextMixin, View):
@@ -34,19 +37,22 @@ class GoogleAuthorizationView(TemplateResponseMixin, ContextMixin, View):
 
     def get(self, request, *args, **kwargs):
         flow = self.flow_class.from_client_config(
-            client_config=settings.GOOGLE_CLIENT_SETTINGS, scopes=settings.GOOGLE_SCOPES["PRESENTATIONS"]["readonly"])
-        flow.redirect_uri = request.build_absolute_uri(self.flow_redirect_uri)
+            client_config=settings.GOOGLE_CLIENT_CONFIG, scopes=settings.GOOGLE_SCOPES["PRESENTATIONS"]["readonly"])
+        flow.redirect_uri = request.build_absolute_uri(str(self.flow_redirect_uri))
         authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
 
-        request["state"] = state
+        request.session["state"] = state
 
         return HttpResponseRedirect(authorization_url)
 
-# get_google_token = GetGoogleTokenView.as_view()
+
+google_auth = GoogleAuthorizationView.as_view()
 
 
 class GoogleOAuth2CallbackView(TemplateResponseMixin, ContextMixin, View):
 
+    flow_class = Flow
+    flow_redirect_uri = reverse_lazy('core:get_google_token')
     template_name = "core/get_token.html"
 
     # def get_context_data(self, **kwargs):
@@ -59,10 +65,10 @@ class GoogleOAuth2CallbackView(TemplateResponseMixin, ContextMixin, View):
         state = request.session['state']
 
         flow = self.flow_class.from_client_config(
-            client_config=settings.GOOGLE_CLIENT_SETTINGS,
+            client_config=settings.GOOGLE_CLIENT_CONFIG,
             scopes=settings.GOOGLE_SCOPES["PRESENTATIONS"]["readonly"],
             state=state)
-        flow.redirect_uri = request.build_absolute_uri(self.flow_redirect_uri)
+        flow.redirect_uri = request.build_absolute_uri(str(self.flow_redirect_uri))
 
         code = request.GET.get('code')
 
@@ -70,7 +76,7 @@ class GoogleOAuth2CallbackView(TemplateResponseMixin, ContextMixin, View):
         credentials = flow.credentials
         request.session['credentials'] = self.credentials_to_dict(credentials)
 
-        return HttpResponseRedirect(authorization_url)
+        return HttpResponseRedirect(reverse_lazy("core:test_google_api"))
 
     def credentials_to_dict(self, credentials):
         return {'token': credentials.token,
@@ -81,25 +87,20 @@ class GoogleOAuth2CallbackView(TemplateResponseMixin, ContextMixin, View):
                 'scopes': credentials.scopes}
 
 
-get_google_token = GetGoogleTokenView.as_view()
+get_google_token = GoogleOAuth2CallbackView.as_view()
 
 
 class TestAPIGoogleView(TemplateResponseMixin, ContextMixin, View):
 
-    template_name = "core/get_token.html"
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     print(self.request.GET.get('code'))
-    #     context['status'] = self.request.GET.get('status')
-    #     return context
+    template_name = "core/test_api.html"
 
     def get(self, request, *args, **kwargs):
-        creds = google.oauth2.credentials.Credentials(**request.session['credentials'])
+        creds = Credentials(**request.session['credentials'])
 
         service = build('slides', 'v1', credentials=creds)
 
         # Call the Slides API
+        PRESENTATION_ID = "1sAih7s8HNbfjHyZuBaUxKPtcpBFZ0-FvmL6CdT_XQh4"
         presentation = service.presentations().get(
             presentationId=PRESENTATION_ID).execute()
         slides = presentation.get('slides')
@@ -110,13 +111,5 @@ class TestAPIGoogleView(TemplateResponseMixin, ContextMixin, View):
 
         return self.render_to_response(self.get_context_data())
 
-    def credentials_to_dict(self, credentials):
-        return {'token': credentials.token,
-                'refresh_token': credentials.refresh_token,
-                'token_uri': credentials.token_uri,
-                'client_id': credentials.client_id,
-                'client_secret': credentials.client_secret,
-                'scopes': credentials.scopes}
 
-
-get_google_token = GetGoogleTokenView.as_view()
+test_google_api = TestAPIGoogleView.as_view()
